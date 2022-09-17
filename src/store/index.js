6d2d0725600b4bf982f3b87c1e61679135a58987
Pavanmachareddy <pavanrmacha@gmail.com>
2022-09-18 01:46:38 +0530

    Add cart fixes

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartlistSlice from "./cartlistreducer";
 
 import cartSlice from "./cartreducer";
 
 const store = configureStore({
-    reducer: { cart:cartSlice.reducer},
+    reducer: { cart:cartSlice.reducer, cartlist: cartlistSlice.reducer},
 })
 
 export default store;
